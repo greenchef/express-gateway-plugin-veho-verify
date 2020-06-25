e2743cb3e4edb67250bec7c7c53c04ec9a50f2b2
m0jeda <mojeda1004@gmail.com>
2020-06-25 15:46:36 -0600

    Revert changes for req body parser

--- policies/veho-verify.policy.js
+++ policies/veho-verify.policy.js
@@ -1,7 +1,4 @@
 const crypto = require('crypto');
-const express = require('express');
-const jsonParser = require('express').json();
-const urlEncodedParser = require('express').urlEncoded({ extended: true });
 
 module.exports = {
   name: 'veho-verify',
@@ -9,37 +6,29 @@ module.exports = {
     $id: 'N/A',
     type: 'object',
     properties: {
-      ipList: {
+      apikeys: {
         type: 'array',
       }
     }
   },
   policy: ({ apikeys }) => {
     return async (req, res, next) => {
-      const x = (req, res, next) => {
-        try {
-          const foundApiKey = apikeys.some((apikey) => {
-            console.log("apikey", apikey)
-            const timestamp = req.headers['x-veho-time'];
-            console.log("timestamp", timestamp)
-            const hash = crypto.createHmac('SHA256', apikey);
-            hash.update(`${timestamp}.${JSON.stringify(req.body)}`);
-            const hashDigest = hash.digest('hex');
-            console.log("req.body", req.body)
-            console.log("hashDigest", hashDigest)
-            if (req.headers['x-veho-hmac-sha256'] === hashDigest) {
-              return true;
-            }
-            return false;
-          });
-          foundApiKey ? console.log(' Veho Signature was verified') : console.error(' Veho Signature was not verified');
-        } catch (e) {
-          console.error('Error in veho-verify policy:', e.error);
-          return;
-        }
-        next();
+      try {
+        const foundApiKey = apikeys.some((apikey) => {
+          const timestamp = req.headers['x-veho-time'];
+          const hash = crypto.createHmac('SHA256', apikey);
+          hash.update(`${timestamp}.${JSON.stringify(req.body)}`);
+          if (req.headers['x-veho-hmac-sha256'] === hash.digest('hex')) {
+            return true;
+          }
+          return false;
+        });
+        foundApiKey ? console.log(' Veho Signature was verified') : console.error(' Veho Signature was not verified');
+      } catch (e) {
+        console.error('Error in veho-verify policy:', e.error);
+        return;
       }
-      jsonParser(req, res, () => urlEncoded(req, res, x));
+      next();
     };
   }
 };
