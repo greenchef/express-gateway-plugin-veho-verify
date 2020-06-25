68b246fc9c054c05fbed4cc2d26b1f7a75cac94b
m0jeda <mojeda1004@gmail.com>
2020-06-25 13:42:45 -0600

    Fix policy

--- policies/veho-verify.policy.js
+++ policies/veho-verify.policy.js
@@ -1,4 +1,7 @@
 const crypto = require('crypto');
+const express = require('express');
+const jsonParser = require('express').json();
+const urlEncodedParser = require('express').urlEncoded({ extended: true });
 
 module.exports = {
   name: 'veho-verify',
@@ -13,22 +16,30 @@ module.exports = {
   },
   policy: ({ apikeys }) => {
     return async (req, res, next) => {
-      try {
-        const foundApiKey = apikeys.some((apikey) => {
-          const timestamp = req.headers['x-veho-time'];
-          const hash = crypto.createHmac('SHA256', apikey);
-          hash.update(`${timestamp}.${JSON.stringify(req.body)}`);
-          if (req.headers['x-veho-hmac-sha256'] === hash.digest('hex')) {
-            return true;
-          }
-          return false;
-        });
-        foundApiKey ? console.log(' Veho Signature was verified') : console.error(' Veho Signature was not verified');
-      } catch (e) {
-        console.error('Error in veho-verify policy:', e.error);
-        return;
+      const x = (req, res, next) => {
+        try {
+          const foundApiKey = apikeys.some((apikey) => {
+            console.log("apikey", apikey)
+            const timestamp = req.headers['x-veho-time'];
+            console.log("timestamp", timestamp)
+            const hash = crypto.createHmac('SHA256', apikey);
+            hash.update(`${timestamp}.${JSON.stringify(req.body)}`);
+            const hashDigest = hash.digest('hex');
+            console.log("req.body", req.body)
+            console.log("hashDigest", hashDigest)
+            if (req.headers['x-veho-hmac-sha256'] === hashDigest) {
+              return true;
+            }
+            return false;
+          });
+          foundApiKey ? console.log(' Veho Signature was verified') : console.error(' Veho Signature was not verified');
+        } catch (e) {
+          console.error('Error in veho-verify policy:', e.error);
+          return;
+        }
+        next();
       }
-      next();
+      jsonParser(req, res, () => urlEncoded(req, res, x));
     };
   }
 };
